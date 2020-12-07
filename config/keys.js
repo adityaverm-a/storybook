205078fd2fe13c76917decb4e00fec00427cdf8c
Aditya Verma <aditya.se 403@gmail.com>
2020-12-07 16:26:20 +0530

    handled Auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "mongodb+srv://adi:adi@cluster0.crmgg.mongodb.net/adi?retryWrites=true&w=majority",
+  googleClientID: "170906061597-aveas6p213l1n7ct1h6ns7tf29l41nuu.apps.googleusercontent.com",
+  googleClientSecret:"LSeOsAfz8hJizppwk3DKDVk6"
+}
